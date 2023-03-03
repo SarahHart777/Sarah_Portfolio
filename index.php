3cbbb468efde939d0ab4cedb49331eb4f543ae45
Sarah777 <70162890+OddDogWarrior@users.noreply.github.com>
2023-03-03 15:11:26 -0700

    Created repository

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<link rel = "stylesheet" href = "test.css">
+
+<!-- start with the php file that creates the top and bottom part of the
+website on each page, just do the header, require_once("name of header.php");
+make the header  -->
+<header><title>Hello World</title></header>
+<body>
+    testy
+    <div>
+        hey man take a walk on the wild side
+        
+    </div>
+    </body>
+</html>
